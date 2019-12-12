e8debcdd83aa0806065a3fe7d69be3324b5fc0eb
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2019-12-12 06:57:22 +0000

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu
+
+echo $(pwd)
+cd $HOME/builder
+
+git config --global user.email "rokibhasansagar2014@outlook.com"
+git config --global user.name "rokibhasansagar"
+git config --global color.ui true
+
+git clone -q "https://${GITHUB_TOKEN}@github.com/rokibhasansagar/google-git-cookies.git" &> /dev/null
+bash google-git-cookies/setup_cookies.sh && rm -rf google-git-cookies
+
+mkdir $(pwd)/pbrp && cd pbrp/
+
+repo init -q -u https://github.com/PitchBlackRecoveryProject/manifest_pb.git -b "twrp-6.0" --depth 1;
+time repo sync -c -q --force-sync --no-clone-bundle --no-tags -j32;
+
+rm -rf device/qcom/ && rm -rf hardware/qcom/ && rm -rf device/generic/;
+
+git clone https://github.com/PitchBlackRecoveryProject/android_device_WALTON_Primo_RX5 device/WALTON/Primo_RX5;
+
+export ALLOW_MISSING_DEPENDENCIES=true;
+source build/envsetup.sh;
+lunch omni_Primo_RX5-userdebug;
+make -j$(nproc --all) recoveryimage;
+ghr -t ${GITHUB_TOKEN} -n "Test Release for Primo_RX5" -b "PBRP v2.9.0" -delete v1.0-test "out/target/product/Primo_RX5/PitchBlack*.zip"
